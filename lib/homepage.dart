cf03cf2f5a2235d9f4b5df19e1b7389b81f2060d
prithivichandru <pruthiviraj.c@boodskap.io>
2023-08-14 16:56:31 +0530

    test

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,7 +15,7 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: Text(args.toString()),
+        child: Text('data'),
       ),
     );
   }
